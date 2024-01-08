583ddd4cf3e417f40080c0b9f89994bdd6648d88
Avunu LLC <mail@avu.nu>
2024-01-08 09:16:46 -0500

    latest non-flake version

--- build.sh
+++ build.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
-# Check if the wifi-credentials.env file exists
-if [ -f ./wifi-credentials.env ]; then
-    # Load the WiFi credentials from the file
-    export $(grep -v '^#' ./wifi-credentials.env | xargs)
+# Check if the .env file exists
+if [ -f ./.env ]; then
+    # Load the environment variables from the .env file
+    export $(cat .env | sed 's/#.*//g' | xargs)
+else
+    # Print an error message and exit
+    echo "Error: copy .env.sample to .env and edit it to match your environment"
+    exit 1
 fi
 
-# Define arguments for Nix build command
-buildArgs=()
-
-# If WIFI_SSID and WIFI_PASSWORD are set, add them to build arguments
-if [ -n "$WIFI_SSID" ] && [ -n "$WIFI_PASSWORD" ]; then
-    buildArgs+=(--argstr wifiSSID "$WIFI_SSID")
-    buildArgs+=(--argstr wifiPassword "$WIFI_PASSWORD")
-fi
+# Specify the output filename
+outputFilename="kiosk.iso"
 
 # Run the Nix build with the arguments
-nix build .#defaultPackage.x86_64-linux "${buildArgs[@]}"
+nix-build ./default.nix -A config.system.build.isoImage -I nixos-config=./kiosk.nix -o ${outputFilename}
+# nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config=./nixpkgs.nix -I nixos-config=./kiosk.nix -o ${outputFilename}
